0b7805c5953e4c9c429f575b0b9d17a37fd1a4b0
Natnael Dagnachew <natnaeldagnachew2784@gmail.com>
2024-08-13 12:20:10 +0300

    finito

--- main.js
+++ main.js
@@ -7,39 +7,8 @@ image.addEventListener('mouseover', () => {
 image.addEventListener('mouseout', () => {
     image.style.animationPlayState = 'running'; // Resumes animation when not hovering
 });
+
 // save contact logic
-// document.addEventListener('DOMContentLoaded', function() {
-//     const saveButton = document.querySelector('.save-btn a.save');
-
-//     saveButton.addEventListener('click', function(event) {
-//         event.preventDefault();
-
-//         const vCardData = `
-// BEGIN:VCARD
-// VERSION:3.0
-// FN:Tigist Damte
-// ORG:Cloud 251
-// TEL;TYPE=CELL:+251940793142
-// EMAIL:tigist@cloud251.com
-// URL:http://www.cloud251.com
-// END:VCARD
-//         `.trim();
-
-//         // Create a Blob from the vCard data
-//         const blob = new Blob([vCardData], { type: 'text/vcard' });
-//         const url = URL.createObjectURL(blob);
-
-//         // Create a link element and simulate a click to download the file
-//         const link = document.createElement('a');
-//         link.href = url;
-//         link.download = 'Tigist_Damte_Contact.vcf'; // File name
-//         link.click();
-
-//         // Clean up the URL object
-//         URL.revokeObjectURL(url);
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     const saveButton = document.querySelector('.save-btn a.save');
 
@@ -69,8 +38,9 @@ END:VCARD
 
         // Clean up the URL object
         URL.revokeObjectURL(url);
-
-        // Optionally, provide feedback to the user
-        alert('The contact vCard has been downloaded. Please import it into your contacts.');
+        
+         // Optionally, provide feedback to the user
+         alert('The contact vCard has been downloaded. Please import it into your contacts.');
+    
     });
 });
